499ec1594da2ca762c6f179e332fde31c3af28c8
TechnoPhasePRO <yrajsingh2020@gmail.com>
2021-11-22 14:26:18 +0530

    Final Signup Page

---
+++ server.js
@@ -0,0 +1,53 @@
+var express = require('express');
+var env = require('dotenv').config()
+var ejs = require('ejs');
+var path = require('path');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+const PORT = process.env.PORT || 3000;
+
+var app = express();
+
+mongoose.connect("mongodb+srv://yraj01:yraj01@cluster0.x6n2j.mongodb.net/New-Users?retryWrites=true&w=majority",{useNewUrlParser: true,useUnifiedTopology: true});
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+});
+
+app.use(session({
+  secret: 'work hard',
+  resave: true,
+  saveUninitialized: false,
+  store: new MongoStore({
+    mongooseConnection: db
+  })
+}));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');	
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(__dirname + '/views'));
+
+var index = require('./routes/index');
+app.use('/', index);
+app.use(function (req, res, next) {
+  var err = new Error('File Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.send(err.message);
+});
+
+
+app.listen(PORT, function () {
+  console.log('Server started on 3000');
+});
